278b5ccf13eec7105b83e377122c0d3340df38a7
ozzzih <dhwlgus3031@gmail.com>
2022-07-05 19:26:57 +0900

    java script

---
+++ 06-3/06_source6_6.js
@@ -0,0 +1,9 @@
+function show(){
+    var str="";
+
+    str = str + myform.txt_name.value + "님은" + "\n";
+    str = str + myform.item01.value + "를 \n";
+    str = str + "좋아하십니다.";
+
+    alert(str);
+}
